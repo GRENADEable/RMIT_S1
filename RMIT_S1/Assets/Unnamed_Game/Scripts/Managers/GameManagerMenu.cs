39b03901814159a88b21b690a8282864f3fd6880
GRENADEable <kahtim.dar@gmail.com>
2022-05-02 10:48:40 +1000

    - Created new project folder for folio 2 project.

---
+++ RMIT_S1/Assets/Unnamed_Game/Scripts/Managers/GameManagerMenu.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
+
+namespace Khatim_F2
+{
+    public class GameManagerMenu : MonoBehaviour
+    {
+        #region Serialized Variables
+
+        #region Datas
+
+        #endregion
+
+        #region Events
+
+        #endregion
+
+        #endregion
+
+        #region Private Variables
+
+        #endregion
+
+        #region Unity Callbacks
+
+        #region Events
+        void OnEnable()
+        {
+
+        }
+
+        void OnDisable()
+        {
+
+        }
+
+        void OnDestroy()
+        {
+
+        }
+        #endregion
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+        #endregion
+
+        #region My Functions
+
+        #endregion
+
+        #region Coroutines
+
+        #endregion
+
+        #region Events
+
+        #endregion
+    }
+}
